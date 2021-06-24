bcc61dd909b44c81ce10524fa5e0c6e37398b32b
weblastor@gmail.com <munv@sheridancollege.ca>
2021-06-24 17:00:18 +0600

    got the form done (refactoring included)

---
+++ lib/Screens/TransactionScreen/Widgets/AppBarWidget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'AppBarContentWidget.dart';
+
+class AppBarWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+        backgroundColor: Color(0xFFFFEDD5),
+        expandedHeight: 200.0,
+        title: Text("0dj43334ed",
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0)),
+        leading: Icon(Icons.arrow_back, color: Colors.black),
+        flexibleSpace: AppBarContentWidget());
+  }
+}
