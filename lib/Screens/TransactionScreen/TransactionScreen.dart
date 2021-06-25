5d9831415e2acbc9d78d4b32cb4ab0f8e7d4cd75
weblastor@gmail.com <munv@sheridancollege.ca>
2021-06-25 12:08:10 +0600

    got connection to an api

--- lib/Screens/TransactionScreen/TransactionScreen.dart
+++ lib/Screens/TransactionScreen/TransactionScreen.dart
@@ -1,10 +1,30 @@
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ubexlab_test_ui/Screens/TransactionScreen/Widgets/AppBarWidget.dart';
 import 'package:ubexlab_test_ui/Screens/TransactionScreen/Widgets/FormWidget.dart';
 import 'package:ubexlab_test_ui/Screens/TransactionScreen/Widgets/SetWidget.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class TransactionScreen extends StatelessWidget {
+  void getData() async {
+    Uri url = Uri.parse("http://13.209.43.204:5080/block");
+    var headers = {"Content-Type" : "Application/json-rpc"};
+    var body = json.encode({
+      "jsonrpc": "2.0",
+      "id": "00456",
+      "method": "getTransaction",
+      "params": {
+        "transactionId":
+            "6a28e00918a51336fb09e6f01ce9c056fd9eb452c0701d517c5f1db555ca5821"
+      }
+    });
+    var response = await http.post(url, headers: headers, body: body);
+    Map<String, dynamic> responseMap = json.decode(response.body);
+    print(responseMap);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
